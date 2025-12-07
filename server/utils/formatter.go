4a6398e94490d7df4ea2e9fafbe14b3095f87516
Kartik Sharma <imailkartiksharma@gmail.com>
2025-12-07 20:42:28 +0530

    add formatter util

--- server/utils/formatter.go
+++ server/utils/formatter.go
@@ -1,1 +1,19 @@
 package utils
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func FormatJson(input string) (string, error) {
+	var jsonObj interface{}
+
+	if err := json.Unmarshal([]byte(input), &jsonObj); err != nil {
+		return "", fmt.Errorf("invalid JSON: %v", err)
+	}
+	pretty, err := json.MarshalIndent(jsonObj, "", "  ")
+	if err != nil {
+		return "", fmt.Errorf("error formatting JSON: %v", err)
+	}
+	return string(pretty), nil
+}
