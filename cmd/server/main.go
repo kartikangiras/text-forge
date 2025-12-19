acbe023debb0402b9c28846b49358052758b2b33
Kartik Angiras <angiraskartik@gmail.com>
2025-12-19 23:36:36 +0530

    add server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,32 @@
 package server
+
+import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+)
+
+//go:embed ui/dist
+var uiAssets embed.FS
+
+func main() {
+	distFS, err := fs.Sub(uiAssets, "ui/dist")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`{"status": "ok"}`))
+	})
+
+	fileServer := http.FileServer(http.FS(distFS))
+	mux.Handle("/", fileServer)
+
+	log.Println("Server starting on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}
